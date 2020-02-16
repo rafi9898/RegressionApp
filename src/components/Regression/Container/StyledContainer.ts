121844acd4ca76272ba9b7193cf6fe1f71aa88f5
rafi9898 <r-podraza@wp.pl>
2020-02-16 13:57:07 +0100

    Added add button

--- src/components/Regression/Container/StyledContainer.ts
+++ src/components/Regression/Container/StyledContainer.ts
@@ -5,3 +5,21 @@ export const StyledBox = styled.div`
   height: 100%;
   padding-top: 160px;
 `;
+
+export const StyledAddIcon = styled.span`
+  width: 60px;
+  height: 60px;
+  box-shadow: 0px 3px 6px #00000029;
+  opacity: 1;
+  background-color: #4587df;
+  border-radius: 7px;
+  position: fixed;
+  right: 40px;
+  bottom: 20px;
+  font-size: 40px;
+  font-weight: bold;
+  color: #fff;
+  text-align: center;
+  cursor: pointer;
+  text-shadow: 0px 1px 1px #333;
+`;
